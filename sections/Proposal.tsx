b942cfbdc75b130d2574dcf191ef674ea719a8ff
Nathan Luiz Bezerra Martins <nathan@deco.cx>
2024-04-22 15:03:52 +0000

    Release Staging environment with 6 changes made by Nathan Luiz Bezerra Martins

--- sections/Proposal.tsx
+++ sections/Proposal.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 export default function Section({ title = "Big Title", description = "Description on text" }: Props) {
   return (
-    <div class="flex flex-col px-20 gap-10 py-15">
+    <div class="flex flex-col py-[60px] pl-[150px] pr-[100px] gap-10">
       <h1 class="text-4xl font-bold">{title}</h1>
 
       <p class="text-lg">{
